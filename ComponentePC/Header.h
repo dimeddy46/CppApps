ca3d2454670eabe2195c3ef75a1caece66f495c2
dimeddy46 <47603856+dimeddy46@users.noreply.github.com>
2019-02-17 22:10:29 +0100

    Update Header.h

--- ComponentePC/Header.h
+++ ComponentePC/Header.h
@@ -4,11 +4,8 @@
 #define __HEADER_H__
 
 #define _CRT_SECURE_NO_WARNINGS
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#endif
-#include <windows.h>
 
+#include <windows.h>
 #include <iostream>
 #include <fstream>
 #include <string.h>
