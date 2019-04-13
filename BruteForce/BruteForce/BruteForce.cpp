57a6c017f0a6eadae89bdf40f37f22cfcdf83ac1
unknown <dimeddy97@gmail.com>
2019-04-13 20:31:22 +0300

    updates

--- BruteForce/BruteForce/BruteForce.cpp
+++ BruteForce/BruteForce/BruteForce.cpp
@@ -6,7 +6,7 @@ using namespace std::chrono;
 mutex mu;
 thread thr[16];
 time_point<system_clock> refz;
-const string cautat = "findme1";
+const string cautat = "azeacu";
 const string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
 //const string alphabet = "abcdefghijklmnopqrstuvwxyz";
 
@@ -15,11 +15,10 @@ bool gasit = false;
 unsigned long long int comp;
 
 // ---------------start letter, number of letters, array used for generating permutations -------
-void BruteForce(char startJump, char letCount, int x[32])
+void BruteForce(char startJump, char letCount, int *x)
 {
 	char i, j;
-	unsigned int timp;
-
+	
 	for (i = 0; i <= n && !gasit; i++)
 	{
 		x[startJump] = i;
@@ -30,24 +29,23 @@ void BruteForce(char startJump, char letCount, int x[32])
 				model += alphabet[x[j]];
 
 			if (x[0] != x[letCount - 4])		// output settings(when the 5th letter from right to left changes,
-			{					// it outputs current stats). x[0] = last 5th letter
+			{									// it outputs current stats). x[0] = last 5th letter
 				x[0] = x[letCount - 4];
 				mu.lock();
-				timp = (unsigned int)duration_cast<seconds>(system_clock::now() - refz).count();
-				cout << model << " TIME:" << timp << "s " << " THR: " << this_thread::get_id() << endl;
+				cout << model << " TIME:" << (unsigned int)duration_cast<seconds>(system_clock::now() - refz).count()
+					 << "s " << " THR: " << this_thread::get_id() << endl;
 				mu.unlock();
 			}
 
 			if (model == cautat)	// if starting string matches permutation
 			{
 				mu.lock();
-				timp = (unsigned int)duration_cast<seconds>(system_clock::now() - refz).count();
 				cout << "--------------- A FOST GASIT SIRUL " << model << " in "
-					<< timp << "s ---------------------" << endl;
+					 << duration_cast<seconds>(system_clock::now() - refz).count() 
+					 << "s ---------------------" << endl;
 				gasit = true;
 				mu.unlock();
 			}
-			//comp++;
 		}
 		else BruteForce(startJump + 1, letCount, x);
 	}
